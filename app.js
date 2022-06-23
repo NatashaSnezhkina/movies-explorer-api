540d5695210a0d3de8ba18276f6f100bab574811
Natasha Snezhkina <snezhkina.natasha@yandex.ru>
2022-06-23 13:36:42 +0300

    fixed cors

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ const { errors } = require('celebrate');
 const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes');
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
 
 const { PORT = 3000, NODE_ENV, DB_URL } = process.env;
 const app = express();
 mongoose.connect(`${NODE_ENV === 'production' ? DB_URL : 'mongodb://localhost:27017/bitfilmsdb'}`);
+
+const allowedCors = [
+  'localhost:3000',
+  'https://diploma.natasha.snezh.nomoredomains.xyz',
+  'http://diploma.natasha.snezh.nomoredomains.xyz',
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+];
+
 app.use(requestLogger);
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
@@ -22,6 +31,23 @@ app.use(router);
 app.use(errorLogger);
 app.use(errors());
 app.use(error);
-app.use(cors);
 
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  const { methodHttp } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+  if (methodHttp === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
 app.listen(PORT);
